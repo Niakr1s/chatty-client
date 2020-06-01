4f5fad9e3689021348ec4eab863d4641214bf1c7
Niakr1s <pavel2188@gmail.com>
2020-06-01 16:10:49 +0300

    added suggestion to join chat

--- src/components/ChatComponents/ChatChatList.js
+++ src/components/ChatComponents/ChatChatList.js
@@ -23,17 +23,17 @@ class ChatChatList extends React.Component {
                             let chat = this.props.chats.get(chatname)
                             return <li
                                 className={"userlist-item lightblue cursor-default " +
-                                    `${(this.props.activeChat === chat.chat) && "chat-active"}`}
+                                        `${(this.props.activeChat === chat.chat) ? "chat-active " : ""}`}
                                 key={chatname}
                                 onClick={() => {
                                     if (!chat.joined) { this.props.joinChat(chat.chat) }
                                     else { this.props.setActiveChat(chat.chat) }
                                     this.props.setRead(chat.chat);
                                 }}>{chatname}
-                                {chat.joined && <span
+                                <span
                                     className="float-right joined"
                                     title="unread counter"
-                                >{chat.unread && "new"}</span>}</li> // TODO instead 0 - unread messages
+                                >{chat.joined ? chat.unread && "new" : "join"}</span></li> // TODO instead 0 - unread messages
                         })}
                     </ul>
                     <div className="userlist-footer darkblue"></div>
