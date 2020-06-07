d98e0522ac6ae45a91252128d2e84eeff98ff666
Niakr1s <pavel2188@gmail.com>
2020-06-07 14:00:04 +0300

    impl sticky chat

--- src/components/ChatComponents/ChatBox.js
+++ src/components/ChatComponents/ChatBox.js
@@ -5,16 +5,39 @@ class ChatBox extends React.Component {
     constructor(props) {
         super(props)
 
-        // it's holds ["chatname", "scrollTop"]
+        // it's holds {"chatname": {"scrollTop": number, "stickBottom": bool}
         this.scrolls = new Map()
         this.myRef = React.createRef();
+
+        this.lastChatName = "";
     }
 
     componentDidUpdate = () => {
         if (!this.props.chat) return
-        let lastScrollTop = this.scrolls.get(this.props.chat.chat);
-        if (lastScrollTop === undefined) lastScrollTop = this.myRef.current.scrollHeight;
-        this.myRef.current.scrollTop = lastScrollTop;
+
+        // removing stick botton if we switched chats and updating lastChatName
+        if (this.lastChatName !== "" && this.props.chat.chat != this.lastChatName) {
+            let lastScrollStatus = this.getScrollStatus(this.lastChatName)
+            lastScrollStatus.stickBottom = false;
+            this.scrolls.set(this.lastChatName, lastScrollStatus)
+        }
+        this.lastChatName = this.props.chat.chat
+
+        // scrolling to last scroll height, it will call onScroll event, that will update scrollStatus
+        let scrollStatus = this.getScrollStatus(this.props.chat.chat)
+        if (scrollStatus.stickBottom) {
+            this.myRef.current.scrollTop = this.myRef.current.scrollHeight
+        } else { this.myRef.current.scrollTop = scrollStatus.scrollTop; }
+    }
+
+    getScrollStatus = (chatname) => {
+        let scrollStatus = this.scrolls.get(chatname);
+        if (scrollStatus === undefined) scrollStatus = {
+            scrollTop: this.myRef.current.scrollHeight,
+            stickBottom: true
+        }
+        this.scrolls.set(chatname, scrollStatus)
+        return scrollStatus
     }
 
     render = () => {
@@ -22,7 +45,10 @@ class ChatBox extends React.Component {
             <div className="chatbox overflow-y blue-gradient" ref={this.myRef}
                 onScroll={() => {
                     if (!this.props.chat) return;
-                    this.scrolls.set(this.props.chat.chat, this.myRef.current.scrollTop);
+                    this.scrolls.set(this.props.chat.chat, {
+                        scrollTop: this.myRef.current.scrollTop, //+ this.myRef.current.offsetHeight + 2,
+                        stickBottom: this.myRef.current.scrollTop + this.myRef.current.offsetHeight + 2 >= this.myRef.current.scrollHeight
+                    })
                 }
                 }>
                 {this.props.chat && this.props.chat.messages.map((m, idx, arr) => {
